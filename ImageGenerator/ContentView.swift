ccc214a705eb69d0ccd174a4c0ae8542d8000b83
Radhika K <multicoredump@gmail.com>
2026-03-30 14:12:13 -0700

    Initial Commit

---
+++ ImageGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageGenerator
+//
+//  Created by Radhika Karandikar on 3/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Radhika Karandikar on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
